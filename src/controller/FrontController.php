0a9dab0416b93f46ac83a9985cb60826b0252109
ManariiT-W <162648761+ManariiT-W@users.noreply.github.com>
2024-04-29 16:23:22 -1000

    Update FrontController.php
    enlever le token

--- src/controller/FrontController.php
+++ src/controller/FrontController.php
@@ -28,9 +28,9 @@ class FrontController
     public function home()
         // Epaces Acuueil
     { //echo "coucou";
-       $articles = $this->articleDAO->getArticles();
-       //voir articleDAO pour la requête select
-       return $this->view->render('home', ['articles' => $articles]);
+        $articles = $this->articleDAO->getArticles();
+        //voir articleDAO pour la requête select
+        return $this->view->render('home', ['articles' => $articles]);
     }
 
     public function article($articleId)
@@ -38,24 +38,15 @@ class FrontController
     {
         $article = $this->articleDAO->getArticle($articleId);
         $comments = $this->commentDAO->getCommentsFromArticle($articleId);
-        return $this->view->render('single', ['article' => $article,'comments' => $comments]);
+        return $this->view->render('single', ['article' => $article, 'comments' => $comments]);
     }
 
 
-    public function addComment($articleId, $pseudo, $content) {
-
-//        // Vérifier si le token CSRF soumis correspond à celui stocké dans la session utilisateur
-//        if (!isset($_SESSION['csrfToken']) || $_SESSION['csrfToken'] !== $csrfToken) {
-//            // Rediriger vers une page d'erreur CSRF
-//            header('Location: error_csrf.php');
-//            exit;
-//        }
-
-        // Validation supplémentaire des données du commentaire
-        if (!$this->validateCommentData($articleId, $pseudo, $content) || ) {
-            // Rediriger vers une page d'erreur de validation des données
-            header('Location: error_csrf_Valid.php');
-            exit;
+    public function addComment($articleId, $pseudo, $content)
+    {
+        if (!$this->validateCommentData($articleId, $pseudo, $content)) {
+            $this->errorController->errorNotFound();
+            return;
         }
 
         $this->commentDAO->addComment($articleId, $pseudo, $content);
@@ -64,39 +55,8 @@ class FrontController
         exit;
     }
 
-//    public function addComment($articleId, $pseudo, $content) {
-//        if (!$this->validateCommentData($articleId, $pseudo, $content)) {
-//            $this->errorController->errorNotFound();
-//            return;
-//        }
-//
-//        $this->commentDAO->addComment($articleId, $pseudo, $content);
-//        // Rediriger vers l'article pour voir le commentaire ajouté
-//        header('Location: index.php?route=article&articleId=' . $articleId);
-//        exit;
-//    }
-//
-//    private function validateCommentData($articleId, $pseudo, $content) {
-//    return !empty($pseudo) && !empty($content) && !empty($articleId) && $_SERVER['REQUEST_METHOD'] === 'POST';
-//}
-
-//Génération du CSRF Token
-    private function generateCsrfToken() {
-        $numBitsToken = 128;
-        $tokenBytes = random_bytes($numBitsToken / 8);
-        $csrfToken = bin2hex($tokenBytes);
-
-//        $csrfToken = bin2hex(random_bytes(32)); // Génère un token aléatoire de 32 caractères
-
-        // Stocker le token CSRF dans la session
-        $_SESSION['csrf_token'] = $csrfToken;
-
-        return $csrfToken;
-    }
-
-    public function validateCsrfToken() {
-        return;
+    private function validateCommentData($articleId, $pseudo, $content)
+    {
+        return !empty($pseudo) && !empty($content) && !empty($articleId) && $_SERVER['REQUEST_METHOD'] === 'POST';
     }
 }
-
-?>
